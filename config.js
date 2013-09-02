22bd5125f940247336711a4c6ea783be5ed301f1
tim walker <tim.twalker@gmail.com>
2013-09-01 21:46:09 -0700

    add bower and such

---
+++ config.js
@@ -0,0 +1,30 @@
+/**
+ * RequireJS configuration
+ */
+require.config({
+  //deps: ['main'],
+
+  paths: {
+    // Third-party libraries
+    jquery: 'bower_components/jquery/jquery',
+    underscore: 'bower_components/lodash/lodash',
+    backbone: 'bower_components/backbone/backbone',
+    mustache: 'bower_components/mustache/mustache',
+    requireLib: 'bower_components/requirejs/require',
+    text: 'bower_components/requirejs-text/text',
+    // Unit testing libraries
+    mocha: 'bower_components/mocha/mocha',
+    chai: 'bower_components/chai/chai'
+  },
+
+  shim: {
+    backbone: {
+      deps: ['underscore', 'jquery'],
+      exports: 'Backbone'
+    },
+    mocha: {
+      exports: 'mocha'
+    }
+  }
+
+});
